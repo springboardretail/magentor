2db33d16281094f7a240f8e119cebb163ab524d0
Dyego Costa <dyego@dyegocosta.com>
2015-07-15 15:55:12 -0300

    Allow connection to receive host with protocol

--- lib/magentor/connection.rb
+++ lib/magentor/connection.rb
@@ -9,7 +9,14 @@ module Magento
     end
 
     def client
-      @client ||= XMLRPC::Client.new(config[:host], config[:path], config[:port])
+      host, protocol = normalize_host config[:host]
+      use_ssl = protocol == 'https'
+
+      @client ||= XMLRPC::Client.new_from_hash \
+        host: host,
+        use_ssl: use_ssl,
+        path: config[:path],
+        port: config[:port]
     end
 
     def connect
@@ -22,50 +29,54 @@ module Magento
 
     private
 
-      def connect!
-        logger.debug "call: login"
-        retry_on_connection_error do
-          @session = client.call("login", config[:username], config[:api_key])
-        end
-      end
+    def normalize_host(host)
+      String(host).gsub(/\/$/, '').split('://').reverse
+    end
 
-      def cache?
-        !!config[:cache_store]
+    def connect!
+      logger.debug "call: login"
+      retry_on_connection_error do
+        @session = client.call("login", config[:username], config[:api_key])
       end
+    end
 
-      def call_without_caching(method = nil, *args)
-        logger.debug "call: #{method}, #{args.inspect}"
-        connect
-        retry_on_connection_error do
-          client.call_async("call", session, method, args)
-        end
-      rescue XMLRPC::FaultException => e
-        logger.debug "exception: #{e.faultCode} -> #{e.faultString}"
-        if e.faultCode == 5 # Session timeout
-          connect!
-          retry
-        end
-        raise Magento::ApiError, "#{e.faultCode} -> #{e.faultString}"
-      end
+    def cache?
+      !!config[:cache_store]
+    end
 
-      def call_with_caching(method = nil, *args)
-        config[:cache_store].fetch(cache_key(method, *args)) do
-          call_without_caching(method, *args)
-        end
+    def call_without_caching(method = nil, *args)
+      logger.debug "call: #{method}, #{args.inspect}"
+      connect
+      retry_on_connection_error do
+        client.call_async("call", session, method, args)
       end
+    rescue XMLRPC::FaultException => e
+      logger.debug "exception: #{e.faultCode} -> #{e.faultString}"
+      if e.faultCode == 5 # Session timeout
+        connect!
+        retry
+      end
+      raise Magento::ApiError, "#{e.faultCode} -> #{e.faultString}"
+    end
 
-      def cache_key(method, *args)
-        "#{config[:username]}@#{config[:host]}:#{config[:port]}#{config[:path]}/#{method}/#{args.inspect}"
+    def call_with_caching(method = nil, *args)
+      config[:cache_store].fetch(cache_key(method, *args)) do
+        call_without_caching(method, *args)
       end
+    end
 
-      def retry_on_connection_error
-        attempts = 0
-        begin
-          yield
-        rescue EOFError
-          attempts += 1
-          retry if attempts < 2
-        end
+    def cache_key(method, *args)
+      "#{config[:username]}@#{config[:host]}:#{config[:port]}#{config[:path]}/#{method}/#{args.inspect}"
+    end
+
+    def retry_on_connection_error
+      attempts = 0
+      begin
+        yield
+      rescue EOFError
+        attempts += 1
+        retry if attempts < 2
       end
+    end
   end
 end
